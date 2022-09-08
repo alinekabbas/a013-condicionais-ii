d165fefa230391b73e80c5c8183f18792345fc2d
alinekabbas <alinekabbas@gmail.com>
2022-09-08 12:40:45 -0300

    Exercicios 2 e 3 feitos

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,24 @@
-//código a ser reescrito
+//Reescreva os códigos do arquivo script.js utilizando o if ternário.
 
-if(nome === "José"){
-	console.log("Oi, Zé!");
-} else {
-	console.log("Olá, "+nome);
-}
+//1.
+const nome = prompt("Me diga seu nome")
 
-if(idade >= 18){
-	console.log("pode tirar carteira de motorista!");
-} else {
-	console.log("Ainda não pode tirar carteira de motorista");
-}
+// if(nome === "José"){
+// 	console.log("Oi, Zé!");
+// } else {
+// 	console.log("Olá, "+nome);
+// }
+
+nome === "José" ? console.log("Oi, Zé!") : console.log("Olá, "+nome)
+
+//2.
+
+const idade = Number(prompt("Qual é a sua idade?"))
+
+// if(idade >= 18){
+// 	console.log("pode tirar carteira de motorista!");
+// } else {
+// 	console.log("Ainda não pode tirar carteira de motorista");
+// }
+
+idade >= 18 ? console.log("pode tirar carteira de motorista!") : console.log("Ainda não pode tirar carteira de motorista")
